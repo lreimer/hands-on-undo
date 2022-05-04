03dd1d0ab17c2087802cacb36c33d528a130fde8
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-05-05 00:06:30 +0200

    Removed initialization in default constructor. Use @PostConstruct.

--- services/vehicle-service/src/main/java/hands/on/undo/vehicle/VehicleRepository.java
+++ services/vehicle-service/src/main/java/hands/on/undo/vehicle/VehicleRepository.java
@@ -12,10 +12,6 @@ public class VehicleRepository {
 
     private Map<String, Vehicle> vehicles = new HashMap<>();
 
-    public VehicleRepository() {
-        initialize();
-    }
-
     @PostConstruct
     public void initialize() {
         vehicles.put("WVWZZZ1JZ3W386752", new Vehicle("WVWZZZ1JZ3W386752", "VW", "1J", "2003"));
